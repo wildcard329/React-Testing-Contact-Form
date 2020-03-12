da474a680059cd223c69e5107a3b860c9908268c
wildcard329 <enkiduwildman@gmail.com>
2020-03-12 12:55:02 -0700

    rendering test on contactform passed

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ContactForm from "./ContactForm";
+
+// test("first name in contactform", () => {
+//     const { getByText } = render(<ContactForm />);
+//     const name = getByText('First Name*')
+//     expect(name).toBeInTheDocument()
+//   });
+
