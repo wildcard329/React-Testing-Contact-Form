05c3a42c636a64b825b3c18862f5b55d38e4278d
wildcard329 <enkiduwildman@gmail.com>
2020-03-12 12:37:33 -0700

    checked to see if first name would pass or fail for app component, expected and received fail

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,11 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  const { getByText } = render(<App />);
 });
+
+// test("renders App without crashing", () => {
+//   const { getByText } = render(<App />);
+//   const name = getByText('First Name')
+//   expect(name).toBeInTheDocument()
+// });
